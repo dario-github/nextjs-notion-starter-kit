383c2e18b799346072fa6d41b090e1e44bb265d5
Dario Zhang <zdclink@gmail.com>
2022-07-08 17:00:55 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '03c610019e8040ff856fccaee4e7ca98',
-
+  // rootNotionPageId: '03c610019e8040ff856fccaee4e7ca98',
+  rootNotionPageId: '1b43a5e041a84d079565cb503c91100e',
+  
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Dario Blog',
+  name: "Dario's Blog",
   domain: 'blog2.dariolink.vercel.app',
   author: 'Dario Zhang',
 
